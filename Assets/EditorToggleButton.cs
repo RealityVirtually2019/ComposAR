1c83fe66f8f8837833680c3efe9f43009792fa56
Nicholas714 <grananicholas@gmail.com>
2019-01-18 12:19:12 -0500

    started rotate, scale, and transform buttons

---
+++ Assets/EditorToggleButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+enum EditorMode { Scale, Rotate, Move, Unselected };
+
+public class EditorToggleButton : MonoBehaviour {
+
+    private EditorMode mode = EditorMode.Unselected;
+
+    public GameObject floor;
+    public Slider scaleSlider;
+
+	void Start () {
+        scaleSlider.onValueChanged.AddListener(delegate { ScaleValueChange(); });
+	}
+
+    // Menu Toggles
+
+    public void OnClickScale() {
+        mode = EditorMode.Scale;
+    }
+
+    public void OnClickRotate() {
+        mode = EditorMode.Rotate;
+    }
+
+    public void OnClickMove() {
+        mode = EditorMode.Move;
+    }
+
+    // Slider Changes
+
+    public void ScaleValueChange() {
+        floor.transform.localScale += new Vector3(0.1F, 0, 0);
+    }
+
+}
