059e4e440fe5835f3fb71b02ce6a8b11810cc0eb
tomthecarrot <code@tomthecarrot.com>
2019-01-17 23:45:41 -0500

    Add Scripts dir & Misc script

---
+++ Assets/Scripts/Misc.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Misc : MonoBehaviour {
+
+	void Awake() {
+		TeleportalProject.Shared.OnTeleportalLoaded = OnTeleportalLoaded;
+	}
+
+	void OnTeleportalLoaded() {
+		// blank
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
