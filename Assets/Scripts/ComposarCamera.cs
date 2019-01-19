ea6dda0f322cafa3629f3bce17d5ecd50e989a7b
tomthecarrot <code@tomthecarrot.com>
2019-01-19 16:24:45 -0500

    Stub ComposarCamera.SaveImage()

--- Assets/Scripts/ComposarCamera.cs
+++ Assets/Scripts/ComposarCamera.cs
@@ -16,14 +16,18 @@ public class ComposarCamera : MonoBehaviour {
 	}
 
 	public Texture2D GrabImage() {
-		// TODO set texture on GUI 'camera preview' rawimage
-		
 		// Convert to Texture2D
 		Texture2D tex = new Texture2D(512, 512, TextureFormat.ARGB32, false);
 		RenderTexture.active = this.rentex;
 		tex.ReadPixels(new Rect(0, 0, this.rentex.width, this.rentex.height), 0, 0);
 		tex.Apply();
 		return tex;
+	}
+
+	public void SaveImage() {
+		Texture2D tex = this.GrabImage();
+		
+		// TODO save to iOS/Android camera rolls
 	}
 	
 }
