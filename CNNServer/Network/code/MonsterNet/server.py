09a7db07639a2773345c19919efe0ae4c402b700
Matt Kelsey <mattkelseymk@gmail.com>
2019-01-19 00:37:51 -0500

    Up to forge, partially working status and jpg conversion

--- CNNServer/Network/code/MonsterNet/server.py
+++ CNNServer/Network/code/MonsterNet/server.py
@@ -4,6 +4,9 @@ import random
 import string
 import main
 import os
+import time
+import re
+from PIL import Image
 
 from flask import Flask, request
 app = Flask(__name__)
@@ -14,8 +17,8 @@ return_val = 'no'
 def doEverything():
   print(request.files)
   for key, file  in request.files.items():
-    file.save('./../../../Data/CharacterDraw/sketch/m1/' + file.filename)
-  # main.main()  
+    file.save('./../../../Data/CharacterDraw/sketch/' + file.filename)
+  main.main()  
   global return_val
   gevent.joinall([gevent.spawn(spawned)])
   return return_val
@@ -26,7 +29,7 @@ def spawned():
   payload = {'client_id': os.environ['FORGE_CLIENT_ID'], \
             'client_secret': os.environ['FORGE_CLIENT_SECRET'], \
             'grant_type': 'client_credentials', \
-            'scope': 'data:write'}
+            'scope': 'data:write data:read'}
   resp = requests.post('https://developer.api.autodesk.com/authentication/v1/authenticate', \
                 headers={'Content-Type': 'application/x-www-form-urlencoded'}, \
                 data=payload)
@@ -34,36 +37,65 @@ def spawned():
   
   print("token: " + access_token)
 
+  jsonHeaders = {'content-type': 'application/json', 'Authorization': 'Bearer ' + access_token}
+
   name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
   payload2 = {'scenename': name, 'format': 'rcm'}
   resp2 = requests.post('https://developer.api.autodesk.com/photo-to-3d/v1/photoscene', \
-                headers={'content-type': 'application/json', 'Authorization': 'Bearer ' + access_token}, \
+                headers=jsonHeaders, \
                 data=payload2)
   # print("error, if there was one otherwise None: " + resp2.json()['Error'])
   
   print(resp2.json())
   print(resp2.json()['Photoscene']['photosceneid'])
-  return_val = resp2.json()['Photoscene']['photosceneid']
+  sceneId = resp2.json()['Photoscene']['photosceneid']
+  return_val = sceneId
 
 
   headers = {\
+      'Content-Type': 'application/json',\
       'Authorization': 'Bearer ' + access_token,\
   }
 
   files = {\
       'photosceneid': (None, 'hcYJcrnHUsNSPII9glhVe8lRF6lFXs4NHzGqJ3zdWMU\n'),\
-      'type': (None, 'image\n'),\
-      'file[0]': ('./../../../Data/CharacterDraw/sketch/sketch-F-0.png', open('./../../../Data/CharacterDraw/sketch/sketch-F-0.png', 'rb')),\
-      'file[1]': ('./../../../Data/CharacterDraw/sketch/sketch-S-0.png', open('./../../../Data/CharacterDraw/sketch/sketch-S-0.png', 'rb')),\
+      'type': (None, 'image\n')\
+      # 'file[0]': ('./../../../Data/CharacterDraw/sketch/sketch-F-0.png', open('./../../../Data/CharacterDraw/sketch/sketch-F-0.png', 'rb')),\
+      # 'file[1]': ('./../../../Data/CharacterDraw/sketch/sketch-S-0.png', open('./../../../Data/CharacterDraw/sketch/sketch-S-0.png', 'rb')),\
   }
+
+  i = 0
+  for file in os.listdir('./../../../Data/CharacterDraw/output/results/m1/'):
+    print(file)
+    filePath = './../../../Data/CharacterDraw/output/results/m1/' + file
+    im = Image.open(filePath)
+    rgb_im = im.convert('RGB')
+    filePath = re.sub('(\w+).png', '\\1.jpg', filePath)
+    rgb_im.save(filePath)
+    files['file[' + str(i) + ']'] = (filePath, open(filePath, 'rb'))
+    i+=1
+  print(files)
+
   response = requests.post('https://developer.api.autodesk.com/photo-to-3d/v1/file', headers=headers, files=files)
   print(response)
 
-  # payload3 = {'photosceneid': sceneId, 'type': 'image', \
-  #             'file[0]':}
-  # resp3 = requests.post('https://developer.api.autodesk.com/photo-to-3d/v1/file', \
-  #               headers={'content-type': 'application/json', 'Authorization': 'Bearer ' + access_token}, \
-  #               params=payload3)
+  startResponse = requests.post('https://developer.api.autodesk.com/photo-to-3d/v1/photoscene/' + sceneId, headers=headers)
+  print(startResponse.json())
+
+  progressResponse = None
+  while progressResponse is None or progressResponse.json()['Photoscene']['progressmsg'] != 'DONE':
+    progressResponse = requests.get('https://developer.api.autodesk.com/photo-to-3d/v1/photoscene/' + sceneId + '/progress', \
+                headers={'Content-Type': 'application/json', 'Authorization': 'Bearer ' + access_token})
+    # print(progressResponse)
+    # print(progressResponse.json())
+    print(progressResponse.json()['Photoscene']['progress'])
+    time.sleep(7)
+  
+  fileResponse = requests.get('https://developer.api.autodesk.com/photo-to-3d/v1/photoscene/' + sceneId + '?format=fbx', \
+                headers=jsonHeaders)
+
+  print(fileResponse.json()['Photoscene']['scenelink'])
+
   
 if __name__ == '__main__':
     app.run()
