477f44cac2b971c96f275720f5f0fabe81b705df
tomthecarrot <code@tomthecarrot.com>
2019-01-19 00:56:55 -0500

    Add Render Texture -> composar.camera

--- Assets/Scripts/ComposarCamera.cs
+++ Assets/Scripts/ComposarCamera.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class ComposarCamera : MonoBehaviour {
 
-	// Use this for initialization
+	private RenderTexture rentex;
+
 	void Start () {
+		this.rentex = new RenderTexture(256, 256, 16, RenderTextureFormat.ARGB32);
+		this.rentex.Create();
+	}
+
+	public RenderTexture GetRenderTexture() {
+		return this.rentex;
+	}
+
+	public Texture2D GrabImage() {
+		// TODO set texture on GUI 'camera preview' rawimage
 		
+		// Convert to Texture2D
+		Texture2D tex = new Texture2D(512, 512, TextureFormat.ARGB32, false);
+		RenderTexture.active = this.rentex;
+		tex.ReadPixels(new Rect(0, 0, this.rentex.width, this.rentex.height), 0, 0);
+		tex.Apply();
+		return tex;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
