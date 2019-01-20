04dbc16b5b07bd63b52ba0fd30605295b57856eb
Sam Roquitte <samroq@yahoo.com>
2019-01-20 01:43:14 -0500

    Add ability to save image

--- Assets/Scripts/DrawBackgroundController.cs
+++ Assets/Scripts/DrawBackgroundController.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class DrawBackgroundController : MonoBehaviour
 {
 
-    public GameObject BackgroundPlane;
+    // public GameObject BackgroundPlane;
     public Material frontPlane;
-
+    public RenderTexture SaveTexture;
 
     void Start() {
         setBackgroundImage("Assets/test.jpg");
+    }
+
+    public void Save() {
+        setBackgroundImage("Assets/reset.png");
+        StartCoroutine(CoSave());
+    }
+
+    private IEnumerator CoSave() {
+        yield return new WaitForEndOfFrame();
+
+        RenderTexture.active = SaveTexture;
+
+        var texture2D = new Texture2D(SaveTexture.width, SaveTexture.height);
+        texture2D.ReadPixels(new Rect(0, 0, SaveTexture.width, SaveTexture.height), 0, 0);
+        texture2D.Apply();
+
+        var data = texture2D.EncodeToPNG();
+        File.WriteAllBytes(Application.dataPath + "/savedImage.png", data);
     }
 
     public void setBackgroundImage(string filePath) {
@@ -26,13 +45,12 @@ public class DrawBackgroundController : MonoBehaviour
         frontPlane.mainTexture = tex;
 
         // Apply to plane
-        MeshRenderer mr = BackgroundPlane.GetComponent<MeshRenderer>();
+        MeshRenderer mr = this.GetComponent<MeshRenderer>();
         mr.material = frontPlane;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
