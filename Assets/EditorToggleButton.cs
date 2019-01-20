86ec629dba564f78e56937e9b0c0619ae224d1d6
Nicholas714 <grananicholas@gmail.com>
2019-01-20 10:23:06 -0500

    changes

--- Assets/EditorToggleButton.cs
+++ Assets/EditorToggleButton.cs
@@ -43,9 +43,6 @@ public class EditorToggleButton : MonoBehaviour {
     private GameObject actuallySelectedObject;
     private ComposarCamera highlightedCamera;
 
-    // TODO: EnableRender DisableRender in ComposerCamera (where grabImage is)
-    // TODO: snap rotation scale 
-
     void Awake() {
         EditorToggleButton.Shared = this;
     }
@@ -69,10 +66,10 @@ public class EditorToggleButton : MonoBehaviour {
 
         // they already have an item, so they are moving an object 
         if (selectedItem != null) {
-            selectedItem.gameObject.transform.eulerAngles = selectedObjectEulerAngles;
             // Snap position to actualMovePosition
 
-            if (snapGridToggle.isOn) {
+            if (!snapGridToggle.isOn) {
+                Debug.Log("test");
                 float GridSnapSize = 0.25f; // meters
 
                 Vector3 snappedPosition = actuallySelectedObject.transform.position;
@@ -103,8 +100,10 @@ public class EditorToggleButton : MonoBehaviour {
                 */
 
                 // LOCAL ONLY version - selectedItem.gameObject.transform.position = snappedPosition;
-                TeleportalAr.Shared.MoveItem(selectedItem.Id, snappedPosition.x, snappedPosition.y, snappedPosition.z, 0, 0);
+                TeleportalAr.Shared.MoveItem(selectedItem.Id, snappedPosition.x, snappedPosition.y, snappedPosition.z, selectedObjectEulerAngles.y, selectedObjectEulerAngles.x);
             }
+
+            selectedItem.gameObject.transform.eulerAngles = selectedObjectEulerAngles;
             
             newEditorMode = EditorMode.SelectedObject;
         } else {
@@ -133,13 +132,10 @@ public class EditorToggleButton : MonoBehaviour {
         
         if (currentEditMode == EditorMode.Camera && newMode != EditorMode.Camera) {
             // disable camera 
-<<<<<<< HEAD
-            // lookingAtItem.gameObject.GetComponent<ComposarCamera>().DisableRender();
-=======
             if (highlightedCamera != null) {
                 highlightedCamera.DisableRender();
             }
->>>>>>> 1bbeb1362531f03ab8932f14d0b4f7861ef2cb96
+
         } else if (newMode == currentEditMode) {
             // double check if looking at floor, dup/delete button are deleted 
             if (newMode == EditorMode.LookingAtObject && lookingAtItem != null && !lookingAtItem.gameObject.transform.name.Contains("Floor")) {
@@ -334,6 +330,10 @@ public class EditorToggleButton : MonoBehaviour {
                 TeleportalAr.Shared.ReleaseItem(selectedItem);
             } 
 
+            if (actuallySelectedObject != null)  {
+                Destroy(actuallySelectedObject);
+            }
+            
             selectedItem.gameObject.transform.SetParent(null);
             selectedItem.gameObject.transform.SetParent(floor.transform);
         }
@@ -389,10 +389,10 @@ public class EditorToggleButton : MonoBehaviour {
 
         Vector3 angles = selectedItem.gameObject.transform.eulerAngles;
 
-        float angleSnap = 15; // degrees 
+        float angleSnap = 30; // degrees 
 
-        float snappedAngle = (float) Math.Round(rotateSlider.value / angleSnap) * angleSnap;
-        
+        float snappedAngle = snapGridToggle.isOn ? (float) Math.Round(rotateSlider.value / angleSnap) * angleSnap : rotateSlider.value;
+
         Vector3 rotationVector = new Vector3(
                 rotationDropdown.value == 0 ? snappedAngle : angles.x, 
                 rotationDropdown.value == 1 ? snappedAngle : angles.y,
