4c3914edf3859ce7421c75d7654230d97b1c13e9
tomthecarrot <code@tomthecarrot.com>
2019-01-20 11:28:43 -0500

    High-res image saving & fix draw capture

--- Assets/Scripts/ComposarCamera.cs
+++ Assets/Scripts/ComposarCamera.cs
@@ -9,7 +9,7 @@ public class ComposarCamera : MonoBehaviour {
 
 	void Start () {
 		this.cameraComponent = this.gameObject.transform.Find("camera").GetComponent<Camera>();
-		this.rentex = new RenderTexture(256, 256, 16, RenderTextureFormat.ARGB32);
+		this.rentex = new RenderTexture(2048, 2048, 16, RenderTextureFormat.ARGB32);
 		this.rentex.Create();
 		this.cameraComponent.targetTexture = this.rentex;
 		this.DisableRender();
@@ -29,7 +29,7 @@ public class ComposarCamera : MonoBehaviour {
 
 	public Texture2D GrabImage() {
 		// Convert to Texture2D
-		Texture2D tex = new Texture2D(512, 512, TextureFormat.ARGB32, false);
+		Texture2D tex = new Texture2D(this.rentex.width, this.rentex.height, TextureFormat.ARGB32, false);
 		RenderTexture.active = this.rentex;
 		tex.ReadPixels(new Rect(0, 0, this.rentex.width, this.rentex.height), 0, 0);
 		tex.Apply();
