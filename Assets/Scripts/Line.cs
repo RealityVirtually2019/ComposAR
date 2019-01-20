777135e17135755b855302e38945525ab51bcac7
Sam Roquitte <samroq@yahoo.com>
2019-01-19 19:31:41 -0500

    Remove debug logging

--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -19,14 +19,12 @@ public class Line : MonoBehaviour
             if (((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) || Input.GetMouseButton(0))) {
                 Plane objPlane = new Plane(Camera.main.transform.forward*-1, this.transform.position);
 
-                Debug.Log(Input.mousePosition);
                 Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
                 float rayDistance;
                 if (objPlane.Raycast(mRay, out rayDistance)) {
                     this.transform.position = mRay.GetPoint(rayDistance);
                 }
             } else if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) || Input.GetMouseButtonUp(0)) {
-                Debug.Log("Stop Draw");
                 active = false;
             }
             yield return null;
